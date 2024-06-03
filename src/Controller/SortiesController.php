df8a27c456fcffca7c958a64c2eeb66ad08225c2
Hélène Deblock <helene.deblock2023@campus-eni.fr>
2024-06-03 16:49:56 +0200

    ajout du SortiesController et de sa vue

---
+++ src/Controller/SortiesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SortieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route('/sorties', name: 'sortie_')]
+
+class SortiesController extends AbstractController
+{
+    #[Route('', name: 'list')]
+
+    public function list(SortieRepository $sortieRepository): Response
+    {
+        $sorties = $sortieRepository->findAll();
+
+        return $this->render('sorties.html.twig', [
+            'sorties' => $sorties
+        ]);
+    }
+}
