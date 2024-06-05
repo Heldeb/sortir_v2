bd283da30cf08ca3aa140fec3862f3b621bf8ec8
Lynda IHADADENE <lynda.oumaouche@hotmail.fr>
2024-06-05 10:20:41 +0200

    Creation de la page creation d'une sortie

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SortiesController extends AbstractController
 {
     #[Route('/sorties', name: 'vue_sorties')]
-
     public function sorties(SortieRepository $sortieRepository): Response
     {
         $sorties = $sortieRepository->findAll();
@@ -20,4 +23,21 @@ class SortiesController extends AbstractController
         ]);
     }
 
+    #[Route('/sorties/create', name: 'creation_sorties')]
+    public function creation(Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+        $sortieForm->handleRequest($request);
+
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+        }
+        return $this->render('sorties/create.html.twig', [
+            'sortieForm' => $sortieForm->createView()
+        ]);
+    }
 }
+
